694f4269b76af064bdb842c41292d18f035ee7b9
Nadezhda <temurazova.n@gmail.com>
2024-07-15 01:16:01 +0400

    deleted tellg()

--- main.cpp
+++ main.cpp
@@ -21,7 +21,6 @@ int main() {
     std::string answer;
     std::string expersAnswer;
 
-
     std::vector<int> played;
     int expertPoints = 0;
     int viewerPoints = 0;
@@ -29,16 +28,13 @@ int main() {
     fileA.open("../answers.txt");
 
     while (expertPoints != 13 || viewerPoints != 13) {
-
         std::cout << "Choose sector:" << std::endl;
         std::cin >> input;
         while (input < 1 || input > 13) {
             std::cout << "Error input. Try again." << std::endl;
             std::cin >> input;
         }
-        if (expertPoints == 0 && viewerPoints == 0) {
-            offset = 1;
-        }
+
         offset += input;
 
         if (offset > 13) {
@@ -48,16 +44,14 @@ int main() {
         answer = readXLine(fileA, offset);
 
         std::cout << question << std::endl;
-        std::cout << "we are here:  " << fileQ.tellg() << std::endl;
 
-
-         std::cout << "Please enter your answer in one word" << std::endl;
-         std::cin >> expersAnswer;
-         if (expersAnswer == answer) {
-             expertPoints++;
-         } else {
-             viewerPoints++;
-         }
+        std::cout << "Please enter your answer in one word" << std::endl;
+        std::cin >> expersAnswer;
+        if (expersAnswer == answer) {
+            expertPoints++;
+        } else {
+            viewerPoints++;
+        }
     }
     fileQ.close();
     fileA.close();
